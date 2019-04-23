5b4fe862b408b889222e7b1272bdddae474cbead
R K <blah@eample.com>
2019-04-23 12:09:08 +0200

    Updates

---
+++ precache-manifest.63bcb90188029bfd2186f5c40dcc0a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a60c5f4cff193673a4",
+    "url": "/redux-dictionary/static/js/runtime~main.eab14f62.js"
+  },
+  {
+    "revision": "d8764f69e666acd82f0b",
+    "url": "/redux-dictionary/static/js/main.99009ea2.chunk.js"
+  },
+  {
+    "revision": "a7d9cd1247245a824dce",
+    "url": "/redux-dictionary/static/js/2.15adf957.chunk.js"
+  },
+  {
+    "revision": "d8764f69e666acd82f0b",
+    "url": "/redux-dictionary/static/css/main.8f3db1ac.chunk.css"
+  },
+  {
+    "revision": "a7d9cd1247245a824dce",
+    "url": "/redux-dictionary/static/css/2.086b400f.chunk.css"
+  },
+  {
+    "revision": "51043e1872deab629ed1913ba1dae5e0",
+    "url": "/redux-dictionary/index.html"
+  }
+];
